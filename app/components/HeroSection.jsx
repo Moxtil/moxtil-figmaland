18ad0b446196113d5b8f1280f310b8d5011d305e
Moxtil <sang.j52016@gmail.com>
2025-12-22 02:33:23 +0300

    Snowfall

--- app/components/HeroSection.jsx
+++ app/components/HeroSection.jsx
@@ -7,7 +7,7 @@ import Snowfall from "react-snowfall";
 export default function HeroSection() {
   return (
     <section className="relative w-full min-h-screen flex items-center justify-center px-6 py-20 hero">
-      <Snowfall color="#eee" />
+      <Snowfall enable3DRotation={true}  color="#eee" />
 
       {/* Background Glow */}
       <div className="absolute inset-0 -z-10 flex items-center justify-center">
